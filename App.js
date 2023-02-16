02d0be720a6f994404323caf21288bfd643c06bd
Pujanprasai <contact.1pujanprasai@gmail.com>
2023-02-15 20:45:26 -0500

    Added bottom tab navigator

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+  <SafeAreaView style={styles.container}> 
+    <Text>Settings</Text>
+  </SafeAreaView>
+);
+const Map = () => (
+  <SafeAreaView style={styles.container}>
+    <Text>Map</Text>
+  </SafeAreaView>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -21,8 +38,48 @@ export default function App() {
   }
   return (
     <>
-      <RestaurantScreen />
-      <ExpoStatusBar style="auto" />
+       <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === "Restaurants") {
+                  iconName = "md-restaurant";
+                } else if (route.name === "Settings") {
+                  iconName = "md-settings";
+                } else if (route.name === "Map") {
+                  iconName = "md-map";
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray",
+            }}
+          >
+            <Tab.Screen name="Restaurants" component={RestaurantScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      <StatusBar style="auto" />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 10,
+  },
+  listItem: {
+    marginBottom: 10,
+  },
+});
